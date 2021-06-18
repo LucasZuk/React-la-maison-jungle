1c9082d3548daac6ab2238919d6e6299a91fc60e
LucasZuk <lucas.zukervar@gmail.com>
2021-06-18 16:15:48 +0200

    Add event

--- src/component/PlantItem.js
+++ src/component/PlantItem.js
@@ -9,7 +9,7 @@ class PlantItem extends (React.Component) {
 
         const plant = this.props.plant;
         return (
-            <li className='lmj-plant-item' onClick={sparkles}>
+            <li className='lmj-plant-item' onClick={()=> sparkles(plant.name)}>
                 <img className='lmj-plant-item-cover' src={plant.cover} />
                 <PlantName plant={plant}/>
                 <div>
@@ -21,8 +21,9 @@ class PlantItem extends (React.Component) {
     }   
 }
 
-function sparkles() {
-    console.log('✨ Ceci est un clic ✨');
+function sparkles(name) {
+    
+    alert(`Tu veux acheter de la bonne ${name} ? Vas-y fais toi plaiz !`);
 }
 
 export default PlantItem;
